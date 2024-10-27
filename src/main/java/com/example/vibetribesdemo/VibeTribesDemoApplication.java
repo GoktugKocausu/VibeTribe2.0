1b2b90a57bc7867d1c737f07a7ca0d15326e2c94
GoktugKocausu <92365954+GoktugKocausu@users.noreply.github.com>
2024-10-27 23:04:42 +0300

    Add .gitattributes to handle line endings

---
+++ src/main/java/com/example/vibetribesdemo/VibeTribesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.vibetribesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeTribesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VibeTribesDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vibetribesdemo/VibeTribesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vibetribesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeTribesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
